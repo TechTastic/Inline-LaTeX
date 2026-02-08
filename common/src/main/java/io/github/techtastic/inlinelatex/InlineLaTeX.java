822c903a9b0a28cb69c09cf4a87a7d90ca42ab70
TechTastic <thetechnomancer123@gmail.com>
2026-02-08 07:30:34 -0600

    This should be working...

--- common/src/main/java/io/github/techtastic/inlinelatex/InlineLaTeX.java
+++ common/src/main/java/io/github/techtastic/inlinelatex/InlineLaTeX.java
@@ -7,29 +7,27 @@ import com.samsthenerd.inline.api.matching.*;
 import com.samsthenerd.inline.utils.URLSprite;
 import net.minecraft.resources.ResourceLocation;
 
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.regex.Pattern;
 
 public final class InlineLaTeX {
     public static final String MOD_ID = "inlinelatex";
-    private static final Pattern LATEX_PATTERN = Pattern.compile("\\[(?:tex|latex|formula):(.*?)]");
-    private static final String LATEX_API_URL = "https://latex.codecogs.com/png.image?dpi{200}";
+    private static final Pattern LATEX_PATTERN = Pattern.compile("\\[(?:tex|latex|formula):([^]]+)]");
+    private static final String LATEX_API_URL = "https://latex.codecogs.com/png.image?\\dpi{200}";
+    private static final RegexMatcher MATCHER = new RegexMatcher.Simple(
+            LATEX_PATTERN,
+            of("latex"),
+            matchResult -> new InlineMatch.DataMatch(new SpriteInlineData(new URLSprite(URLEncoder.encode(LATEX_API_URL + matchResult.group(1), StandardCharsets.UTF_8), of("latex")))),
+            MatcherInfo.fromId(of("latex"))
+    );
 
     public static void init() {
-        InlineAPI.INSTANCE.addChatMatcher(new RegexMatcher.Simple(
-                LATEX_PATTERN,
-                of("latex"),
-                matchResult -> new InlineMatch.DataMatch(new SpriteInlineData(new URLSprite(LATEX_API_URL + matchResult.group(), of("latex")))),
-                MatcherInfo.fromId(of("latex"))
-        ));
+        InlineAPI.INSTANCE.addChatMatcher(MATCHER);
     }
 
     public static void initClient() {
-        InlineClientAPI.INSTANCE.addMatcher(new RegexMatcher.Simple(
-                LATEX_PATTERN,
-                of("latex"),
-                matchResult -> new InlineMatch.DataMatch(new SpriteInlineData(new URLSprite(LATEX_API_URL + matchResult.group(), of("latex")))),
-                MatcherInfo.fromId(of("latex"))
-        ));
+        InlineClientAPI.INSTANCE.addMatcher(MATCHER);
     }
 
     public static ResourceLocation of(String path) {
