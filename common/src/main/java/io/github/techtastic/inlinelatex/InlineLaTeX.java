a5f7fe89006dd14bea82f53f7657699bee571499
TechTastic <74630543+TechTastic@users.noreply.github.com>
2026-02-08 15:14:34 -0600

    IT WORKS

--- common/src/main/java/io/github/techtastic/inlinelatex/InlineLaTeX.java
+++ common/src/main/java/io/github/techtastic/inlinelatex/InlineLaTeX.java
@@ -7,6 +7,10 @@ import com.samsthenerd.inline.api.matching.*;
 import com.samsthenerd.inline.utils.URLSprite;
 import net.minecraft.resources.ResourceLocation;
 
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URL;
+import java.net.URLConnection;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.util.regex.Pattern;
@@ -14,11 +18,11 @@ import java.util.regex.Pattern;
 public final class InlineLaTeX {
     public static final String MOD_ID = "inlinelatex";
     private static final Pattern LATEX_PATTERN = Pattern.compile("\\[(?:tex|latex|formula):([^]]+)]");
-    private static final String LATEX_API_URL = "https://latex.codecogs.com/png.image?\\dpi{200}";
+    private static final String LATEX_API_URL = "https://latex.codecogs.com/png.latex?";
     private static final RegexMatcher MATCHER = new RegexMatcher.Simple(
             LATEX_PATTERN,
             of("latex"),
-            matchResult -> new InlineMatch.DataMatch(new SpriteInlineData(new URLSprite(URLEncoder.encode(LATEX_API_URL + matchResult.group(1), StandardCharsets.UTF_8), of("latex")))),
+            matchResult -> new InlineMatch.DataMatch(new SpriteInlineData(new URLSprite(toURL(matchResult.group(2)), of("latex/" + matchResult.group(2).hashCode())), true)),
             MatcherInfo.fromId(of("latex"))
     );
 
@@ -32,5 +36,9 @@ public final class InlineLaTeX {
 
     public static ResourceLocation of(String path) {
         return new ResourceLocation(MOD_ID, path);
+    }
+
+    public static String toURL(String formula) {
+        return LATEX_API_URL + URLEncoder.encode("\\dpi{200}\\fg{FFFFFF} %s".formatted(formula), StandardCharsets.UTF_8);
     }
 }
