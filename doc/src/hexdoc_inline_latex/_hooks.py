934a4c88b3ed9491517c870af3be288a310ffe6f
TechTastic <74630543+TechTastic@users.noreply.github.com>
2026-02-10 08:35:06 -0600

    Doing recommended changes

--- doc/src/hexdoc_inline_latex/_hooks.py
+++ doc/src/hexdoc_inline_latex/_hooks.py
@@ -1,7 +1,5 @@
 from importlib.resources import Package
 from typing_extensions import override
-import os
-import re
 
 from hexdoc.plugin import (
     HookReturn,
@@ -15,6 +13,21 @@ import hexdoc_inline_latex
 
 from .__gradle_version__ import FULL_VERSION, GRADLE_VERSION
 from .__version__ import PY_VERSION
+
+def replace_latex(match) -> str:
+	full = match.group()
+	delimiter = match.group(1)
+	formula = match.group(2)
+	size = match delimiter:
+		case ",":
+			"\\tiny"
+		case "!":
+			"\\large"
+		case "+":
+			"\\LARGE"
+		case _:
+			"\\small"
+	return f"<img src=\"https://latex.codecogs.com/png.latex?\\dpi{{300}}\\fg{{FFFFFF}}\\{size}\\\\{formula}\" alt=\"{full}\">"
 
 
 class InlinelatexPlugin(ModPluginImpl):
@@ -58,26 +71,10 @@ class InlinelatexModPlugin(ModPlugin):
     def jinja_template_root(self) -> tuple[Package, str]:
         return hexdoc_inlinelatex, "_templates"
 
-    def replace_latex(match) -> str:
-		full = match.group()
-		delimiter = match.group(1)
-		formula = match.group(2)
-		size = match delimiter:
-			case ",":
-				"\\tiny"
-			case "!":
-				"\\large"
-			case "+":
-				"\\LARGE"
-			case _:
-				"\\small"
-		return f"<img src=\"https://latex.codecogs.com/png.latex?\\dpi{{300}}\\fg{{FFFFFF}}\\{size}\\\\{formula}\" alt=\"{full}\">"
-
     @override
     def post_render_book(self, template_args: dict[str, Any], output_dir: Path) -> None:
 		"""Called once per language, after all book files for that language are rendered."""
-		site = open(output_dir + "/index.html", "r").read()
-		site = re.sub(r'\[(?:tex|latex|formula)([:;,!+])([^]]+)\]', replace_latex, site)
-		f = open(output_dir + "/index.html", "x")
-		f.write(site)
-		f.close()
+		site_path = output_dir / "index.html"
+        site = site_path.read_text()
+        site = re.sub(r'\[(?:tex|latex|formula)([:;,!+])([^]]+)\]', replace_latex, site)
+        site_path.write_text(site)
